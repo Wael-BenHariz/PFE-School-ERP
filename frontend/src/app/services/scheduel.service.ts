da905b969ae118da0c9e3879a2e3043d034f63f1
3bouda <abdelaliiheb@gmail.com>
2023-06-14 23:43:01 +0100

    scheduel basic

--- frontend/src/app/services/scheduel.service.ts
+++ frontend/src/app/services/scheduel.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
 
 
 @Injectable({
@@ -8,5 +9,13 @@ import {HttpClient} from "@angular/common/http";
 export class ScheduelService {
 
   constructor(private http: HttpClient) {
+  }
+  createRealisation(classId: number | undefined, subjectId: number | undefined, teacherId: number | undefined, year: number | undefined) {
+    return this.http.post(`${environment.apiUrl}/realisations`, {
+      year: year,
+      classId: classId,
+      subjectId: subjectId,
+      teacherId: teacherId
+    }, {observe: "response"})
   }
 }
